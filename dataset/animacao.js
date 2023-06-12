4c160ee4c2e4f015dbbc7eb53dcb36368ee9f829
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2023-06-12 05:21:55 -0300

    primeiro commit

---
+++ dataset/animacao.js
@@ -0,0 +1,91 @@
+function initTabNav() {
+  const tabMenu = document.querySelectorAll("[data-tab='menu'] li");
+  const tabContent = document.querySelectorAll("[data-tab='content'] section");
+
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      const direcao = tabContent[index].dataset.anime;
+      tabContent[index].classList.add("ativo", direcao);
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(
+    "[data-anime='accordion'] dt"
+  );
+
+  function activeAccordion(event) {
+    // console.log(event.currentTarget);
+    // console.log(this.nextElementSibling);
+    this.classList.toggle("ativo");
+    this.nextElementSibling.classList.toggle("ativo");
+  }
+
+  accordionList.forEach((item) => {
+    item.addEventListener("click", activeAccordion);
+  });
+}
+
+initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll(
+    "[data-menu='suave'] a[href^= '#']"
+  );
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      blck: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll("[data-anime='scroll']");
+  const windowsMetade = window.innerHeight * 0.6;
+
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowsMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimacaoScroll();
---
+++ dataset/script.js
@@ -0,0 +1,7 @@
+// adicione um  atributo data-anime="show-down" e data-anime="show-right" a todos os section's com descrição dos animais
+
+// Utilizando estes atributos, adiciona a classe show-down  ou show-right a sua respectiva section assim que a mesma aparecer na tela (animacao tab)
+
+// No CSS faça com que a show-down anime de cima para baixo e show-right continue  com a mesma  animação  da esquerda para a direita
+
+// substitua todas as classes js- por data atributes.
